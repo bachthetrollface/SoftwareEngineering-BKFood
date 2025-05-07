08a82f8cca3069e2ba8ea2fb74b0c50e0349fff5
bachthetrollface <bachthetrollface@gmail.com>
2025-05-07 12:27:27 +0700

    add js for profile page

---
+++ bkfood/profilepage/static/header_logged.js
@@ -0,0 +1,26 @@
+const community = document.getElementById("community");
+const hiddenElement = document.querySelector(".subnav");
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+community.addEventListener("mouseover", () => {
+  hiddenElement.style.display = "block";
+});
+hiddenElement.addEventListener("mouseover", () => {
+  hiddenElement.style.display = "block";
+});
+
+community.addEventListener("mouseout", () => {
+  hiddenElement.style.display = "none";
+});
+hiddenElement.addEventListener("mouseout", () => {
+  hiddenElement.style.display = "none";
+});
